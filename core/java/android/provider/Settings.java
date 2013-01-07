ab0fa2b338893fda19659add33dac0eb7b061269
Danesh M <daneshm90@gmail.com>
2013-01-07 12:08:00 -0600

    AudioService: launch default music player on headset connect [1/2]
    Change-Id: Ibbb0607a56ab4d9246c14632eb4199558866854a
    
    Do not start music app when headset is unplugged
    
    Change-Id: Ic56ba6c30a72deaf119da40e65ca83a9dcc35c43
    
    AudioService: don't launch music player while in call
    
    If the option to launch the music player when inserting headphones is
    enabled, do not launch the music player if the user is in a call, or
    receiving one, or making one.
    
    Change-Id: I1e1d173fdd246154299b77227f0e1cbed449246b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    AudioService: Prevent crash when there is no music app
    
    Change-Id: I45f22430a926da9b9254de8b58acd304b21fad41

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3985,6 +3985,12 @@ public final class Settings {
        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Whether to launch default music player when headset plugged in
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
