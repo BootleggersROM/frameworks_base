d64eb148b65ee1982436f0e6b719eee1def03534
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 01:56:16 +0000

    Configurable 0, 90, 180 and 270 degree rotation
    Change-Id: Ia7bf8cb64258e1d602230a8f9ea227d3b56a4dab
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4281,6 +4281,12 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_FLASHLIGHT = "global_actions_flashlight";
 
         /**
+         * Allow all rotations.
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
