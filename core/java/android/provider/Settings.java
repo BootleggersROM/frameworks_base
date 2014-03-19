c1b074fe651ab86341df796d5e95fa33e072b7e0
cphelps76 <cphelps76@droidconcepts.com>
2014-03-19 16:23:35 +0000

    base: Add back battery bar [1/2]
    {x} Adds Battery bar with following features:
    {x} aligment
    {x} thickness
    {x} charging animation
    
    @mydongistiny: updated for oreo
    
    Change-Id: Icdfe932868c636ee766b440dd4a911f34203f063
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5637,6 +5637,32 @@ public final class Settings {
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_COLOR = "battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_THICKNESS = "battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_STYLE = "battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
