507e33912a9cd4981f2c7a5bbb5d99603f64964d
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    systemui: Forward-port notification counters
    Change-Id: I39f8b17de493bb7b9e15d14b3b38d6aace64da80

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5046,6 +5046,12 @@ public final class Settings {
         public static final String LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
