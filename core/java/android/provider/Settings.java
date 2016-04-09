18425b6bd7172b8610748495d8fe3f5a2bbaa141
Lokesh Chamane <lokesh.c703@gmail.com>
2016-04-09 19:54:40 -0400

    [1/2] base: HeadsUp snooze function
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    *pretty much a rewrite using the logic from my heads up timeout port
    *also added a second toast for 1 minute instead of saying 1 minutes
    included Make Heads Up snooze toast more informative by beanstown106 too
    *tell the user which app is snoozing
    *also center the toast to account for big app names
    *RU
    Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Forward ported to 10 by: @eyosen
    
    Change-Id: Ia6d0ab44eb49ba4f70a345150f7c40d64105e691

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4783,6 +4783,16 @@ public final class Settings {
 
 
 
+       /**
+        * Defines the global heads up notification snooze
+        * @hide
+        */
+        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
+
+        /** @hide */
+        private static final Validator HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR =
+              new SettingsValidators.InclusiveIntegerRangeValidator(0, 1200000);
+
         /**
          * Whether to show a floating CPU info over the screen
          * @hide
@@ -4861,7 +4871,8 @@ public final class Settings {
             FINGERPRINT_SUCCESS_VIB,
             SCREEN_OFF_ANIMATION,
             LOCKSCREEN_MEDIA_METADATA,
-            LOCKSCREEN_MEDIA_BLUR
+            LOCKSCREEN_MEDIA_BLUR,
+            HEADS_UP_NOTIFICATION_SNOOZE
         };
 
         /**
@@ -5022,6 +5033,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_BLUR);
             PRIVATE_SETTINGS.add(SHOW_CPU_OVERLAY);
+            PRIVATE_SETTINGS.add(HEADS_UP_NOTIFICATION_SNOOZE);
         }
 
         /**
@@ -5128,6 +5140,7 @@ public final class Settings {
             VALIDATORS.put(LOCKSCREEN_MEDIA_METADATA, LOCKSCREEN_MEDIA_METADATA_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_MEDIA_BLUR, LOCKSCREEN_MEDIA_BLUR_VALIDATOR);
             VALIDATORS.put(SHOW_CPU_OVERLAY, SHOW_CPU_OVERLAY_VALIDATOR);
+            VALIDATORS.put(HEADS_UP_NOTIFICATION_SNOOZE,HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR);
         }
 
         /**
