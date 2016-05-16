cae2929e7df5410390b057292f3b471a537c6b96
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to qs tiles [1/2]
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5110,6 +5110,20 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -13037,3 +13051,4 @@ public final class Settings {
         return packages[0];
     }
 }
+
