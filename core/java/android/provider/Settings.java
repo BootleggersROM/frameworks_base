0e178ed1b276f9f369ef2d067ab324c57203bf16
ezio84 <brabus84@gmail.com>
2017-02-16 22:47:17 +0100

    Wired/BT headset: make ringtone audio focus customizable [1/2]
    Allow to choose where to play incoming call ringtones from
    if a wired or BT headset is plugged in.
    Mode 0: play the ringtone from the headset but only if music is playing (if no
    music, play from speakerphone)
    Mode 1 (default aosp behavior): always play the ringtone from both speakerphone
    and headset
    
    thanks @xyyx for unlinking the code form the Telecomm tree

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10302,6 +10302,12 @@ public final class Settings {
                 "location_settings_link_to_permissions_enabled";
 
         /**
+         * Audio focus mode for ringtones when headset is connected
+         * @hide
+         */
+        public static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
