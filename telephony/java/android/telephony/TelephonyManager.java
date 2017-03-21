d9433248ee7005f771540c0fe56e7abbe41ba603
maxwen <max.weninger@gmail.com>
2017-03-21 17:58:33 +0100

    Suspend Actions [1/3]
    ***************************
    Ported from JB by @faust93
    Added screen on/off delay feature by @faust93
    
    Change-Id: Ia8d2bed56c883f43051da4699bf5b19760650623
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3770,6 +3770,16 @@ public class TelephonyManager {
     }
 
     /**
+     * @hide
+     */
+    public void toggle2G(boolean twoGees) {
+        try {
+            getITelephony().toggle2G(twoGees);
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
      * Returns the MMS user agent.
      */
     public String getMmsUserAgent() {
