430cbd6dbe48b007426b614d253dbfb333ce2218
xyyx <xyyx@mail.ru>
2017-09-09 15:12:48 +0800

    base: Lockscreen Charging info  (3/3)
     based on: [2/2] frameworks: show charging current & voltage on lockscreen by
     yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
     yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
     4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
    AICPfy:
    - default set to false
    - Current formatting improvement for devices reporting directly in Amps
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4251,6 +4251,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+        * Whether to show the battery info on the lockscreen while charging
+        * @hide
+        */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
