20c57e01686abb8709d916e451c0fdf3f052dbf1
maxwen <max.weninger@gmail.com>
2017-09-21 13:15:18 +0200

    base: SystemUI: add QS rows config
    with inspiration from @ezio84
    
    Change-Id: I980a1ab2ed024f467367b222ab408ccfe1e1882f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4166,7 +4166,7 @@ public final class Settings {
 
          /** @hide */
         private static final Validator OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE_VALIDATOR =
-                ANY_STRING_VALIDATOR;
+                ANY_INTEGER_VALIDATOR;
 
          /**
          * @hide
@@ -4175,7 +4175,7 @@ public final class Settings {
 
          /** @hide */
         private static final Validator OMNI_QS_LAYOUT_COLUMNS_VALIDATOR =
-                ANY_STRING_VALIDATOR;
+                ANY_INTEGER_VALIDATOR;
 
         /**
          * Whether to display qs tile titles in the qs panel
@@ -4186,6 +4186,15 @@ public final class Settings {
         /** @hide */
         private static final Validator OMNI_QS_TILE_TITLE_VISIBILITY_VALIDATOR =
                 BOOLEAN_VALIDATOR;
+
+        /**
+         * @hide
+         */
+        public static final String OMNI_QS_LAYOUT_ROWS = "qs_layout_rows";
+
+        /** @hide */
+        private static final Validator OMNI_QS_LAYOUT_ROWS_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4250,6 +4259,7 @@ public final class Settings {
             OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE,
             OMNI_QS_LAYOUT_COLUMNS,
             OMNI_QS_TILE_TITLE_VISIBILITY,
+            OMNI_QS_LAYOUT_ROWS,
         };
 
         /**
@@ -4367,6 +4377,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_COLUMNS);
             PRIVATE_SETTINGS.add(OMNI_QS_TILE_TITLE_VISIBILITY);
+            PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_ROWS);
         }
 
         /**
@@ -4460,6 +4471,7 @@ public final class Settings {
                     OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE_VALIDATOR);
             VALIDATORS.put(OMNI_QS_LAYOUT_COLUMNS, OMNI_QS_LAYOUT_COLUMNS_VALIDATOR);
             VALIDATORS.put(OMNI_QS_TILE_TITLE_VISIBILITY, OMNI_QS_TILE_TITLE_VISIBILITY_VALIDATOR);
+            VALIDATORS.put(OMNI_QS_LAYOUT_ROWS, OMNI_QS_LAYOUT_ROWS_VALIDATOR);
         }
 
         /**
