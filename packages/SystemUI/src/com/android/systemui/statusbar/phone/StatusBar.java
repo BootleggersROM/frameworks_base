8beb46b7721a2f4669a8f42a599b52360acaaeed
maxwen <max.weninger@gmail.com>
2017-10-12 20:24:01 +0200

    FWB: add status bar logo (1/2)
    Change-Id: I86eb407557197229481940e565ab711eb56312b0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6409,6 +6409,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         }
 
+        @Override
+            public void onChange(boolean selfChange) {
+        }
+
         public void update() {
             updateDozeBrightness();
             setLockscreenMediaMetadata();
