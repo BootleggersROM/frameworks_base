85f2820d75458267fc1b961c323e8a89aa2384c6
Prema Chand Alugu <premaca@gmail.com>
2017-10-21 06:08:29 +0530

    Logo: Add more Logo Styles [1/2]
    * The styles are controlled through WolvesDen settings
    * This commit is rework of few commits I did in Validus Nougat.
    * The logos are taken from Validus Nougat, prepared by @scoobyjenkins
    
    Bootleggers edit:
    * Added our logos
    * Rebrand to bootleggers
    * Added fixes from https://github.com/ValidusOs/frameworks_base/commit/a00ec25be25b4b2f58290d04bdc64642f03001cf
    >>>>>>
    * Removed previous icons and add a bunch of icons from materialdesignicons.com and also the linux icon from crdroid (credits to @neobuddy89)
    * Adapt to the existing pie code
    
    Change-Id: I5eea1f8f2be485d4ae13212cf112dfabb15ce676

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5863,6 +5863,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_LOGO = "status_bar_logo";
 
+       /**
+        * Statusbar logo Style
+        * @hide
+         */
+        public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
