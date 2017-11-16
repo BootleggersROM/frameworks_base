269dd1572a085966badbcb69b1b64afbdf688900
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-11-15 21:21:31 -0500

    QS: Add Reboot/Recovery QS Tile
    Vectorized Reboot/Recovery QS tile drawables Alex Cruz <du.alexcruz@gmail.com>
    - Resized but reused the power menu reboot icon to keep things uniformed
    - Created a recovery vector that fits the same style too
    
    BenzoEdit: Updated priv-app whitelist to fix boot
    
    Bootleggers edit: Let's recycle the strings of powermenu because of... come on, why have 2 strings of the same?
    
    Change-Id: I0762ffb2b3ddf8c833662a237bde20581e8cc614
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ThemeTile;
@@ -107,6 +108,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ThemeTile(mHost);
             case "gaming":
                 return new GamingModeTile(mHost);
+            case "reboot":
+                return new RebootTile(mHost);
         }
 
         // Intent tiles.
