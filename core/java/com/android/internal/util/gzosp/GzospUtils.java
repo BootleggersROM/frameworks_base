3566ad77814888cd150719827a07e22bababb966
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    Append app name to screenshot file
    kanging Samsung idea :O
    
     *  gzosp: added the activity to utils
    
    Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7

--- core/java/com/android/internal/util/gzosp/GzospUtils.java
+++ core/java/com/android/internal/util/gzosp/GzospUtils.java
@@ -16,8 +16,10 @@
 
 package com.android.internal.util.gzosp;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -31,6 +33,7 @@ import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
 import android.view.WindowManager;
 
+import java.util.List;
 import java.util.Locale;
 
 public class GzospUtils {
@@ -124,6 +127,22 @@ public class GzospUtils {
 
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 
     // Omni Switch Constants
