8ef9e6f88395ae7700cd9b25db2f4797e56c50e3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-28 21:42:07 -0200

    KeyguardHostView: Dismiss keyguard automatically when face is recognized
    Inspired from OP5T behavior
    
    Change-Id: If45c01af8885d57b90980551f929309101b7929d

--- packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
@@ -65,6 +65,8 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
     private OnDismissAction mDismissAction;
     private Runnable mCancelAction;
 
+    private KeyguardUpdateMonitor mKeyguardUpdateMonitor;
+
     private final KeyguardUpdateMonitorCallback mUpdateCallback =
             new KeyguardUpdateMonitorCallback() {
 
@@ -126,6 +128,7 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
 
     public KeyguardHostView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mKeyguardUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         KeyguardUpdateMonitor.getInstance(context).registerCallback(mUpdateCallback);
     }
 
