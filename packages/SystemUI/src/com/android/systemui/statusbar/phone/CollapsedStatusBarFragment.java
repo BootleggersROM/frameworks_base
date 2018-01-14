bf20e8aa820a343b2375d5bd6f08e847025fbaea
Altaf-Mahdi <altaf.mahdi@gmail.com>
2018-01-14 07:49:02 -0500

    Keyguard/Statusbar Carrier label options
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    
    PureNexus Edits:
    * adapted to replace the carrier label toggle in PureNexus-OLD/android_frameworks_base@16a247a
    * this now provides more robust options for carrier label customization
    
    AICP brought up to O by AliB:
    - Adapted to O
    
    Change-Id: I6a73a15160c8334f90feb3a26906a13071097ed7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -255,8 +255,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if ((diff1 & DISABLE_NOTIFICATION_ICONS) != 0) {
             if ((state1 & DISABLE_NOTIFICATION_ICONS) != 0) {
                 hideNotificationIconArea(animate);
+                hideCarrierName(animate);
             } else {
                 showNotificationIconArea(animate);
+                showCarrierName(animate);
             }
         }
     }
@@ -314,6 +316,18 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             animateShow(mBootlegLogo, animate);
         }
         animateShow(mCenterClockLayout, animate);
+    }
+
+    public void hideCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            animateHide(mCustomCarrierLabel, animate, true);
+        }
+    }
+
+    public void showCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            setCarrierLabel(animate);
+        }
     }
 
     /**
@@ -408,6 +422,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mShowCarrierLabel = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_CARRIER, 1,
                 UserHandle.USER_CURRENT);
+        setCarrierLabel(animate);
 
         switch(mLogoStyle) {
                 // Small BTLG
@@ -472,5 +487,12 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 animateHide(mBootlegLogo, animate, false);
             }
         }
+    private void setCarrierLabel(boolean animate) {
+        if (mShowCarrierLabel == 2 || mShowCarrierLabel == 3) {
+            animateShow(mCustomCarrierLabel, animate);
+        } else {
+            animateHide(mCustomCarrierLabel, animate, false);
+        }
     }
+  }
 }
