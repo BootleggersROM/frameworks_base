35f16db5d028f219dc8180cfaec9c30d0cf73fd7
ezio84 <brabus84@gmail.com>
2018-02-01 16:49:32 +0100

    Battery light: Add api to force show led lights
    We can use this to show a led color preview when using the
    color picker, or any other stuff.
    
    use -1 as color int to switch off
    it will be: use 0 as color int to switch off
    
    Change-Id: Id59f480e74a30a875c6640fdfc2e746d98a8513c

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4268,6 +4268,16 @@ public class NotificationManagerService extends SystemService {
             Slog.e(TAG, "exiting pullStats: bad request");
             return 0;
         }
+
+        @Override
+        public void forceShowLedLight(int color) {
+            forceShowLed(color);
+        }
+
+        @Override
+        public void forcePulseLedLight(int color, int onTime, int offTime) {
+            forcePulseLed(color, onTime, offTime);
+        }
     };
 
     @VisibleForTesting
@@ -6069,6 +6079,22 @@ public class NotificationManagerService extends SystemService {
         }
 
         return true;
+    }
+
+    private void forceShowLed(int color) {
+        if (color != -1) {
+            mNotificationLight.setColor(color);
+        } else {
+            mNotificationLight.turnOff();
+        }
+    }
+
+    private void forcePulseLed(int color, int onTime, int offTime) {
+        if (color != -1) {
+            mNotificationLight.setFlashing(color, Light.LIGHT_FLASH_TIMED, onTime, offTime);
+        } else {
+            mNotificationLight.turnOff();
+        }
     }
 
     @GuardedBy("mNotificationLock")
