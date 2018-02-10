274fed304db408eb8890c39d24f3310f9094db1c
El Dainosor <eldainosor@gmail.com>
2018-02-10 04:43:30 -0300

    The fixening 02
    - Solved some branding issues
    - Solved some animation required things

--- packages/SystemUI/src/com/android/systemui/omni/DaylightHeaderProvider.java
+++ packages/SystemUI/src/com/android/systemui/omni/DaylightHeaderProvider.java
@@ -51,7 +51,7 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.android.systemui.R;
-import com.android.internal.util.aicp.AicpUtils;
+import com.android.internal.util.gzosp.GzospUtils;
 
 public class DaylightHeaderProvider implements
         StatusBarHeaderMachine.IStatusBarHeaderProvider {
@@ -373,7 +373,7 @@ public class DaylightHeaderProvider implements
 
     @Override
     public Drawable getCurrent(final Calendar now) {
-        if (!AicpUtils.isPackageAvailable(mPackageName, mContext)) {
+        if (!GzospUtils.isPackageAvailable(mPackageName, mContext)) {
             Log.w(TAG, "Header pack no longer available - loading default " + mPackageName);
             loadDefaultHeaderPackage();
         }
