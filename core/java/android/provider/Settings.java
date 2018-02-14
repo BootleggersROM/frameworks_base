984aea397620568186154744a6d3419496af57ff
El Dainosor <eldainosor@gmail.com>
2018-02-14 00:29:28 -0300

    Revert "[1/2] base: config to disable power menu on secure lock screen"
    This reverts commit daa3f81d2ef6afc2b12bbc58a449b9b20552885f.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8139,13 +8139,6 @@ public final class Settings {
         public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
 
         /**
-         * Disable power menu on secure lock screens
-         *
-         * @hide
-         */
-        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
-
-        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
