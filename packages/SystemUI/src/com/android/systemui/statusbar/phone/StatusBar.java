358e0a8d35a7de0b52d5eb3ea2082db8e16153bb
SpiritCroc <spiritcroc@gmail.com>
2018-02-23 15:33:58 +0100

    Music ticker: once per track is enough
    Remember the track that last was ticker-ed, and only ticker again
    if actually something changed, instead of spamming with tickers
    so that the status bar gets unusable.
    
    Change-Id: I5af6a08ba7c3375fc55f49720dcc2e69c88e2603

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -691,6 +691,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mNavigationBar != null) {
                 mNavigationBar.setMediaPlaying(false);
             }
+            if (mTicker != null) {
+                mTicker.resetShownMediaMetadata();
+            }
         }
     }
 
