630b6e283b9994d2cddec84e1a4865df9efeee6d
Varun Date <date.varun123@gmail.com>
2018-03-30 19:13:03 +0530

    Keyguard & Bouncer Alpha [1/2]
    Forward Port from RR Nougat.
    
    Android Oreo bringup made by @SuperDroidBond

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4797,6 +4797,18 @@ public final class Settings {
         public static final String SCREENSHOT_EDIT_USER_APP = "screenshot_edit_user_app";
 
 
+    	/**
+         * change lockscreen alpha color
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALPHA = "lockscreen_alpha";
+
+        /**
+         * change lockscreen security alpha color
+         * @hide
+         */
+        public static final String LOCKSCREEN_SECURITY_ALPHA = "lockscreen_security_alpha";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6751,6 +6751,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_FOOTER_WARNINGS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ALPHA),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_SECURITY_ALPHA),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -6814,7 +6820,13 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QS_FOOTER_WARNINGS))) {
                 setQsPanelOptions();
-            }
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ALPHA))) {
+              setNewOverlayAlpha();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_SECURITY_ALPHA))) {
+              setSecurityAlpha();
+             }
         }
 
         @Override
@@ -6833,6 +6845,24 @@ public class StatusBar extends SystemUI implements DemoMode,
             setForceAmbient();
             setStatusBarWindowViewOptions();
             setQsPanelOptions();
+            setNewOverlayAlpha();
+            setSecurityAlpha();
+        }
+    }
+
+    public void setNewOverlayAlpha() {
+        float overlayalpha = Settings.System.getFloatForUser(mContext.getContentResolver(),
+        Settings.System.LOCKSCREEN_ALPHA, 0.45f, UserHandle.USER_CURRENT);
+        if (mScrimController != null) {
+        mScrimController.setOverlayAlpha(overlayalpha);
+	}
+    }
+
+    public void setSecurityAlpha() {
+        float securityoverlayalpha = Settings.System.getFloatForUser(mContext.getContentResolver(),
+        Settings.System.LOCKSCREEN_SECURITY_ALPHA, 0.75f, UserHandle.USER_CURRENT);
+        if (mScrimController != null) {
+        mScrimController.setSecurityOverlayAlpha(securityoverlayalpha);
         }
     }
 
