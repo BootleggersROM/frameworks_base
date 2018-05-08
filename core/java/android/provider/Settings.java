8ab80b8a9c76631cbc4aee130cce6be34882d5dd
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 05:33:21 +0000

    Running services icon toggle [1/2]
    aex edits(@ishubhamsingh):- Adapt for oreo
    
    Change-Id: Ic92795891f907110cc40d539a34918d09e2af156
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4853,6 +4853,12 @@ public final class Settings {
         public static final String LOCK_OWNER_FONTS = "lock_owner_fonts";
 
         /**
+         * Whether to show or hide the running services icon
+         * @hide
+         */
+        public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
