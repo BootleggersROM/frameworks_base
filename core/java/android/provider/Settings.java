c5572f788ffbb0d091078e488e54e01e986c6aed
PMS22 <prathams99@rediff.com>
2018-07-25 10:50:07 +0000

    SystemUI: Add Gaming mode tile
    Enabling the tile will :-
    - Show a "First-click" informative dialog
    - Change ringer mode to vibrate
    - Increase the media volume to max
    - Disable Heads Up
    - Turn off Hardware keys
    
    - Turning off the tile will change all the values back to default
    - I haven't enabled Expanded desktop as I dont think any game runs
      in non expanded mode, so better to keep it clean.
    - Duplicate the SystemSetting class from SecureSetting one grabbing
      the System Settings string :-
      https://github.com/FireHound/android_frameworks_base/blob/o8.1/packages/SystemUI/src/com/android/systemui/qs/SecureSetting.java
    
    Change-Id: I6d9440498c0649dad0cdf38ec7c2e25ec610406e
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5087,6 +5087,13 @@ public final class Settings {
         public static final String ALWAYS_HEADSUP_DIALER = "always_headsup_dialer";
 
         /**
+         * Whether to enable gaming mode or not
+         *
+         * @hide
+         */
+        public static final String ENABLE_GAMING_MODE = "enable_gaming_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -34,6 +34,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
@@ -103,6 +104,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("theme")) return new ThemeTile(mHost);
         else if (tileSpec.equals("sync")) return new SyncTile(mHost);
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
+        else if (tileSpec.equals("gaming")) return new GamingModeTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
