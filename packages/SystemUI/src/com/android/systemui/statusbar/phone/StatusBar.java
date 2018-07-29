LineageOS/android_frameworks_base

SystemUI: Avoid creating navbar if it already exists
Change-Id: I47e6cab170266196f62c1958099d510d240a5a68

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -579,7 +579,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             boolean visible = LineageSettings.Global.getIntForUser(mContext.getContentResolver(),
                     LineageSettings.Global.DEV_FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) == 1;
 
-            if (visible) {
+            if (visible && mNavigationBarView == null) {
                 createNavigationBar();
             } else if (mNavigationBarView != null) {
                 mWindowManager.removeViewImmediate(mNavigationBarView);

BootleggersROM/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1213,7 +1213,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 DUActionUtils.hasNavbarByDefault(mContext) ? 1 : 0) != 0;
         if (DEBUG)
             Log.v(TAG, "hasNavigationBar=" + showNav);
-        if (showNav) {
+        if (showNav && mNavigationBarView == null) {
             createNavigationBar();
         }
 