d99aaffe8882fd50083e7b1576a2d34974014fb3
bigrushdog <randall.rushing@gmail.com>
2018-08-21 09:57:11 +0800

    NavigationBar visible from DUI: Initial checkin for Oreo

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8531,6 +8531,12 @@ public final class Settings {
         public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
 
         /**
+         * Add or remove software navigation bar
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_VISIBLE = "navigation_bar_visible";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
