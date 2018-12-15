2aac14461cfef7b3f67e47999fdd4cd12adced0d
spezi77 <spezi7713@gmx.net>
2018-12-15 13:00:36 +0100

    SlimRecents: Add helpers.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ core/java/com/android/internal/util/bootleggers/slimrecents/AbstractIconsHandler.java
@@ -0,0 +1,9 @@
+package com.android.internal.util.bootleggers.slimrecents;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
+
+public interface AbstractIconsHandler {
+    public Drawable getIconFromHandler(Context context, ActivityInfo info);
+}
