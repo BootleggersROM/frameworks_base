8220e141e5b061b449bf9d96dd38c607797d8755
Alex Cruz <du.alexcruz@gmail.com>
2019-02-27 23:31:00 -0300

    BootlegUtils: Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1

--- core/java/com/android/internal/util/bootleggers/BootlegUtils.java
+++ core/java/com/android/internal/util/bootleggers/BootlegUtils.java
@@ -147,6 +147,25 @@ public class BootlegUtils {
         return sDeviceType;
     }
 
+    // Check if device has a notch
+    public static boolean hasVisibleNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
+    }
+    
     public static boolean isPackageAvailable(String packageName, Context context) {
         Context mContext = context;
         final PackageManager pm = mContext.getPackageManager();
