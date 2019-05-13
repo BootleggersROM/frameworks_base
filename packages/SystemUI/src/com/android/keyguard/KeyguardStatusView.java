b53cd6a1b591602cc0f493a66e5d47a84529e181
Harsh Shandilya <harsh@prjkt.io>
2019-05-13 15:20:32 +0530

    TypographicClock: Tint top row with wallpaper primary color
    Change-Id: Ia4cf09bdef39fc95a910c9aea2c22f043fcd7d7d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -1418,6 +1418,7 @@ public class KeyguardStatusView extends GridLayout implements
         mCustomClockView.setDark(dark);
         if (mClockSelection == 9) {
             mTextClock.setTextColor(blendedTextColor);
+            mTextClock.setDarkAmount(mDarkAmount);
         }
         updateVisibilities();
     }
