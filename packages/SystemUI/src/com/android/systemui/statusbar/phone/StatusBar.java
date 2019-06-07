b5861da050cfc2a0f0f78d4b7e3ededf6654bced
spezi77 <spezi7713@gmx.net>
2019-06-07 02:43:34 -0300

    Less aggressive way of updating mobile type icon.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6138,6 +6138,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 stockTileStyle();
                 updateTileStyle();
             } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.USE_OLD_MOBILETYPE))) {
+                updateTelephonyIcons();
+            } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
                 setLockscreenDoubleTapToSleep();
             } else if (uri.equals(Settings.System.getUriFor(
@@ -6185,10 +6188,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                     Settings.System.HEADS_UP_BLACKLIST_VALUES);
             splitAndAddToArrayList(mBlacklist, blackString, "\\|");
             updateTheme();
-            USE_OLD_MOBILETYPE = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.USE_OLD_MOBILETYPE, 0,
-                    UserHandle.USER_CURRENT) != 0;
-            TelephonyIcons.updateIcons(USE_OLD_MOBILETYPE);
+            updateTelephonyIcons();
             setLockscreenDoubleTapToSleep();
             setQsPanelOptions();
             updateTickerAnimation();
@@ -6239,6 +6239,13 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         if (isAmbientContainerAvailable()) {
             ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(null, false);
         }
+    }
+
+    private void updateTelephonyIcons() {
+        USE_OLD_MOBILETYPE = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.USE_OLD_MOBILETYPE, 0,
+            UserHandle.USER_CURRENT) != 0;
+        TelephonyIcons.updateIcons(USE_OLD_MOBILETYPE);
     }
 
     private void setAmbientVis() {
