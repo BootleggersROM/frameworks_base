13876974b71e7e8dca22e5cb0f92e5fa2bca7d58
LuK1337 <priv.luk@gmail.com>
2019-07-16 23:09:24 +0200

    Too long to explain, will be squashed anyway
    Change-Id: I3ad34385fbdaa38692ba5009ebb3872e97ac3907

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -101,7 +101,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_TOGGLE_NAVIGATION_EDITOR      = 51 << MSG_SHIFT;
     private static final int MSG_DISPATCH_NAVIGATION_EDITOR_RESULTS = 52 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH_STATE     = 53 << MSG_SHIFT;
-    private static final int MSG_IN_DISPLAY_FINGERPRINT        = 54 << MSG_SHIFT;
+    private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 54 << MSG_SHIFT;
+    private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 55 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -182,7 +183,8 @@ public class CommandQueue extends IStatusBar.Stub {
         default void toggleNavigationEditor() {}
         default void dispatchNavigationEditorResults(Intent intent) {}
         default void toggleCameraFlashState(boolean enable) { }
-        default void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) { }
+        default void showInDisplayFingerprintView() { }
+        default void hideInDisplayFingerprintView() { }
     }
 
     @VisibleForTesting
@@ -622,13 +624,16 @@ public class CommandQueue extends IStatusBar.Stub {
     }
 
     @Override
-    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
+    public void showInDisplayFingerprintView() {
         synchronized (mLock) {
-            SomeArgs args = SomeArgs.obtain();
-            args.arg1 = show;
-            args.arg2 = isEnrolling;
-            mHandler.obtainMessage(MSG_IN_DISPLAY_FINGERPRINT, args)
-                    .sendToTarget();
+            mHandler.obtainMessage(MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
+        }
+    }
+
+    @Override
+    public void hideInDisplayFingerprintView() {
+        synchronized (mLock) {
+            mHandler.obtainMessage(MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
         }
     }
 
@@ -914,11 +919,14 @@ public class CommandQueue extends IStatusBar.Stub {
                         mCallbacks.get(i).toggleCameraFlashState(msg.arg1 != 0);
                     }
                     break;
-                case MSG_IN_DISPLAY_FINGERPRINT:
+                case MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW:
                     for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).handleInDisplayFingerprintView(
-                                (boolean)((SomeArgs)msg.obj).arg1,
-                                (boolean)((SomeArgs)msg.obj).arg2);
+                        mCallbacks.get(i).showInDisplayFingerprintView();
+                    }
+                    break;
+                case MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).hideInDisplayFingerprintView();
                     }
                     break;
             }
