860c2a4a84db5e8c0d90a9a90688749788bc2aa1
SagarMakhar <sagarmakhar@gmail.com>
2019-09-15 18:15:58 +0000

    Revert "Remove unused auto brightness drawables."
    This reverts commit 1f2484b2f790b2f1cd80bda12d75c263331314a6.
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Ibf50fa648bfc6dc4c73355d4168c21ef1687cacc

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -143,6 +143,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         updateResources();
 
         mBrightnessController = new BrightnessController(getContext(),
+                findViewById(R.id.brightness_icon),
                 findViewById(R.id.brightness_slider));
         mDumpController = dumpController;
 
