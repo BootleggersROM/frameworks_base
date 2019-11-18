711ee69bb20bbcdbd48fbc3b7fcce55ede573960
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-11-18 21:37:39 +0100

    Allow to hide arrow for back gesture [1/2]
    @SKULSHADY: Reverse the logic of the toggle
    Change-Id: Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Change-Id: I4c79c61a12929c3b34147fea7c6f5426a4268b85

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4484,6 +4484,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_LAYOUT_ROWS_LANDSCAPE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.SHOW_BACK_ARROW_GESTURE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4520,6 +4523,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setHeadsUpBlacklist();
             setLockscreenDoubleTapToSleep();
             setMaxKeyguardNotifConfig();
+            setHideArrowForBackGesture();
         }
     }
 
@@ -4527,6 +4531,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         mFpDismissNotifications = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS, 0,
                 UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setHideArrowForBackGesture() {
+        if (getNavigationBarView() != null) {
+            getNavigationBarView().updateBackArrowForGesture();
+        }
     }
 
     private void setPulseOnNewTracks() {
