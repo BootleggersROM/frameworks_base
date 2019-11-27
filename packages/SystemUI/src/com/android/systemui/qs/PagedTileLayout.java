41f1639d5eeaaed7ce153707d297736c4f9dabb4
maxwen <max.weninger@gmail.com>
2019-11-27 23:46:26 +0100

    SystemUI: fix qs page num calc algo
    Change-Id: I27fabcd76b00d8549fe919afda864d64379f4882

--- packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -244,8 +244,14 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
 
     private void emptyAndInflateOrRemovePages() {
         final int nTiles = mTiles.size();
-        // We should always have at least one page, even if it's empty.
-        int numPages = Math.max((int) Math.ceil((double) nTiles / mPages.get(0).maxTiles()), 1);
+        int numPages = 0;
+        if (nTiles <= mPages.get(0).maxTiles()) {
+            numPages = 1;
+        } else {
+            numPages = nTiles / mPages.get(0).maxTiles();
+            // Add one more not full page if needed
+            numPages += numPages * mPages.get(0).maxTiles() > nTiles ? 0 : 1;
+        }
 
         final int NP = mPages.size();
         for (int i = 0; i < NP; i++) {
@@ -495,7 +501,8 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
         for (int i = 0; i < mPages.size(); i++) {
             mPages.get(i).updateSettings();
         }
-        distributeTiles();
+        mDistributeTiles = true;
+        requestLayout();
     }
 
     @Override
