bf7a2f180f7466d3e79b52237e172aa116baf280
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 14:15:00 +0300

    SystemUI: Allow customizing footer text [1/2]
    Change-Id: I08b080914dc8041776eaf88333f338d505ed9f88

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5076,6 +5076,15 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * QS footer text
+         * @hide
+         */
+        public static final String FOOTER_TEXT_STRING = "footer_text_string";
+         /** @hide */
+        private static final Validator FOOTER_TEXT_STRING_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5165,7 +5174,8 @@ public final class Settings {
             OMNIJAWS_WEATHER_ICON_PACK,
             OMNI_LOCKSCREEN_WEATHER_ENABLED,
             LOCKSCREEN_WEATHER_STYLE,
-            FOOTER_TEXT_SHOW
+            FOOTER_TEXT_SHOW,
+            FOOTER_TEXT_STRING
         };
 
         /**
@@ -5356,6 +5366,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
             PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_STYLE);
             PRIVATE_SETTINGS.add(FOOTER_TEXT_SHOW);
+            PRIVATE_SETTINGS.add(FOOTER_TEXT_STRING);
         }
 
         /**
@@ -5490,6 +5501,7 @@ public final class Settings {
             VALIDATORS.put(OMNI_LOCKSCREEN_WEATHER_ENABLED,OMNI_LOCKSCREEN_WEATHER_ENABLED_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_WEATHER_STYLE,LOCKSCREEN_WEATHER_STYLE_VALIDATOR);
             VALIDATORS.put(FOOTER_TEXT_SHOW, FOOTER_TEXT_SHOW_VALIDATOR);
+            VALIDATORS.put(FOOTER_TEXT_STRING, FOOTER_TEXT_STRING_VALIDATOR);
         }
 
         /**
