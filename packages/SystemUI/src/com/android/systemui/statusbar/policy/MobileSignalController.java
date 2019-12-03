9aac64a69570923598712e6363d0b7f8dfcc7a5e
Anushek Prasal <anushekprasal@gmail.com>
2019-12-03 16:42:45 +0530

    SystemUI: Add USE_OLD_MOBILETYPE to SettingsObserver
    This fixes the mobile type icons not updating when toggled.
    
    Change-Id: Idf186069710ecf674ca749f811e2596f9fdbd467

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -158,6 +158,9 @@ public class MobileSignalController extends SignalController<
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.SHOW_FOURG_ICON), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.USE_OLD_MOBILETYPE), false,
+                    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
