30928014bea01a9fb6892235622a9a9f42f4a509
00day0 <therandomuser11@gmail.com>
2020-01-28 00:08:35 +0100

    FODCircleView: only move in Y direction
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -463,7 +463,7 @@ public class FODCircleView extends ImageView {
         }
 
         if (mIsDreaming) {
-            mParams.x += mDreamingOffsetX;
+            //mParams.x += mDreamingOffsetX;
             mParams.y += mDreamingOffsetY;
         }
 
