2f66a002f45e6daa2fbf0196e773c38f7aa4434a
Ido Ben-Hur <idoybh2@gmail.com>
2020-02-03 15:48:29 +0200

    SystemUI: Pulse edge light for all doze triggers [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6040,6 +6040,11 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_DURATION = "ambient_notification_light_duration";
 
         /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_PULSE_FOR_ALL = "ambient_light_pulse_for_all";
+
+        /**
          * Enable weather in lockscreen
          * @hide
          */
