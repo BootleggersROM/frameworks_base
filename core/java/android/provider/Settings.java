f22b8a425bdeabb43a135b8d1e7a07000d146fa2
maxwen <max.weninger@gmail.com>
2020-02-13 02:47:09 +0100

    SystemUI: support using notification color for pulse light [1/2]
    optional use the color information of the notification
    as color for notification pulse light
    
    Change-Id: Icf7c24f8c695f29485123c2c941b08bbdabbbbdf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6029,6 +6029,11 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
 
         /**
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_COLOR_AUTOMATIC = "ambient_notification_light_color_automatic";
+
+        /**
          * Enable weather in lockscreen
          * @hide
          */
