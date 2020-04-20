6e6a711599ed97881594ec157de565f07dd3b9a0
tribual <tribual@web.de>
2020-04-20 14:30:50 +0200

    SystemUI: Nuke ColorOS7 recognizing FP animations NR1 [1/2]
    Change-Id: I49465265d4b0b18ac38d31d1b45515c39c29b73c

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -58,7 +58,6 @@ public class FODAnimation extends ImageView {
         R.drawable.fod_pureview_halo_ring_recognizing_anim,
         R.drawable.fod_pureview_molecular_recognizing_anim,
         R.drawable.fod_blue_firework_recognizing_anim,
-        R.drawable.fod_coloros7_1_recognizing_anim,
         R.drawable.fod_coloros7_2_recognizing_anim
     };
 
