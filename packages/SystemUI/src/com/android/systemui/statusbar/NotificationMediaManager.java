eee762c043b7a84c25e1800ceebc63f78ba3eaf4
El Dainosor <eldainosor@gmail.com>
2020-04-23 00:23:09 +0000

    Revert "[1/2] base: Be more gentle with the LS blur amount"
    This reverts commit ad87f59937216ea008201a775b60a94cbb6d03d3.

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -886,8 +886,8 @@ public class NotificationMediaManager implements Dumpable {
 
     private float getLockScreenMediaBlurLevel() {
         float level = (float) Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_MEDIA_BLUR, 250,
-                UserHandle.USER_CURRENT) / 10;
+                Settings.System.LOCKSCREEN_MEDIA_BLUR, 25,
+                UserHandle.USER_CURRENT) / 100;
         return level;
     }
 }
