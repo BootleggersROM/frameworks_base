2dce50077e74b8a47fae879c08b3afe949355d09
Oliver Scott <olivercscott@gmail.com>
2020-10-02 13:40:28 -0400

    base: Bluetooth timeout feature (1/2)
    SDB - adapt for A12 FLAG_MUTABLE
    
    Issue: calyxos#124
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: I83016f6cfc3f061ecdb429f6f9c60ab31f0741cc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17388,6 +17388,12 @@ public final class Settings {
                 "nr_nsa_tracking_screen_off_mode";
 
         /**
+         * The amount of time in milliseconds before bluetooth is turned off
+         * @hide
+         */
+        public static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+
+        /**
          * Whether to show People Space.
          * Values are:
          * 0: Disabled (default)
