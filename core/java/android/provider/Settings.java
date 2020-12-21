ebcd78430df548aa004cb890bb43efa969cf6ae5
mydongistiny <mydongistiny@users.noreply.github.com>
2020-12-21 07:42:15 +0000

    Weather: Add switches to show city/temp on lockscreen [1/2]
    Disable city by default
    Enable temp by default
    also correct the default icon
    
    Change-Id: I8403edfd69492d5a6a588ce7dc4a153528d821cb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5914,6 +5914,16 @@ public final class Settings {
         public static final String AICP_LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_SHOW_TEMP = "lockscreen_weather_show_temp";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_SHOW_CITY = "lockscreen_weather_show_city";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -6053,6 +6063,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
             PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
             PRIVATE_SETTINGS.add(AICP_LOCKSCREEN_WEATHER_STYLE);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_TEMP);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_CITY);
         }
 
         /**
