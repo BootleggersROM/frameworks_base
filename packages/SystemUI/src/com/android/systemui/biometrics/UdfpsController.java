7c59b843baffe90f828c6a819e69c724cb6ab9a0
TheScarastic <warabhishek@gmail.com>
2021-10-06 06:53:09 +0000

    udfps: Add support for udfps on aod without having dedicated sensor
    [ArianK16a]: Introduce onAcquiredVendor because Google has implemented
    onAcquired in UdfpsController which is called by AIDL implementations to
    disable HBM for most acquire messages. Calling this from the HIDL
    implementation too doesn't work because FINGERPRINT_ACQUIRED_TOO_FAST is
    typically sent pretty fast which breaks fingerprint, maybe that's why
    google didn't call the method from the HIDL implementation. Instead add
    onAcquiredVendor which lets the display pulse if the code matches the
    code defined in overlays.
    Add a ContentObserver to track the screen off udfps setting.
    
    Change-Id: I4aa39efe3714bcc40ab3da862809ca9c8974c6bd

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -27,6 +27,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.hardware.biometrics.BiometricFingerprintConstants;
 import android.hardware.biometrics.SensorProperties;
@@ -40,8 +41,10 @@ import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Process;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
+import android.provider.Settings;
 import android.util.Log;
 import android.util.RotationUtils;
 import android.view.LayoutInflater;
@@ -88,6 +91,8 @@ import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.Execution;
 import com.android.systemui.util.time.SystemClock;
 
+import com.android.systemui.R;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -119,6 +124,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     // Minimum required delay between consecutive touch logs in milliseconds.
     private static final long MIN_TOUCH_LOG_INTERVAL = 50;
+
+    private static final String PULSE_ACTION = "com.android.systemui.doze.pulse";
 
     private final Context mContext;
     private final Execution mExecution;
@@ -184,6 +191,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean mOnFingerDown;
     private boolean mAttemptedToDismissKeyguard;
     private final Set<Callback> mCallbacks = new HashSet<>();
+    private final int mUdfpsVendorCode;
+    private boolean mScreenOffUdfpsEnabled;
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -277,6 +286,15 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         mOverlay.onAcquiredGood();
                     }
                 });
+            }
+        }
+
+        @Override
+        public void onAcquiredVendor(int sensorId, int vendorCode) {
+            if (vendorCode == mUdfpsVendorCode && mStatusBarStateController.isDozing()
+                    && (mScreenOffUdfpsEnabled || mScreenOn)) {
+                mContext.sendBroadcastAsUser(
+                        new Intent(PULSE_ACTION), new UserHandle(UserHandle.USER_CURRENT));
             }
         }
 
@@ -792,6 +810,20 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
+
+        mUdfpsVendorCode = context.getResources().getInteger(R.integer.config_udfpsVendorCode);
+        context.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.SCREEN_OFF_UDFPS_ENABLED), false,
+                new ContentObserver(mainHandler) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        mScreenOffUdfpsEnabled =
+                                Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                        Settings.Secure.SCREEN_OFF_UDFPS_ENABLED, 0,
+                                        KeyguardUpdateMonitor.getCurrentUser())
+                                != 0;
+                    }
+                });
     }
 
     /**
