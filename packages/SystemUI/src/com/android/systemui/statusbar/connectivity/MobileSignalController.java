e7c2b07a66e7fbfe9338b89ed6e13dcd85f9c1cb
timjosten <timjosten@gmail.com>
2022-07-28 01:22:27 +0000

    SystemUI: Fix QS expand lag when VoLTE/VoWiFi icons are enabled
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie9e0686d289e4197c3d14dc28bfcae7e1ba16660

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -82,6 +82,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private final String mNetworkNameDefault;
     private final String mNetworkNameSeparator;
     private final ContentObserver mObserver;
+    private final Handler mHandler = new Handler();
     // Save entire info for logging, we only use the id.
     final SubscriptionInfo mSubscriptionInfo;
     private Map<String, MobileIconGroup> mNetworkToIconLookup;
@@ -399,6 +400,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     @Override
     public void notifyListeners(SignalCallback callback) {
+        mHandler.post(() -> {
         // If the device is on carrier merged WiFi, we should let WifiSignalController to control
         // the SysUI states.
         if (mNetworkController.isCarrierMergedWifi(mSubscriptionInfo.getSubscriptionId())) {
@@ -437,6 +439,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 sbInfo.showTriangle,
                 mCurrentState.isDefault);
         callback.setMobileDataIndicators(mobileDataIndicators);
+        });
     }
 
     private QsInfo getQsInfo(String contentDescription, int dataTypeIcon) {
