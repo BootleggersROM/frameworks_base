b557f71f1bd8f15b040ff3122f2e4291a2f5e326
Semavi Ulusoy <doc.divxm@gmail.com>
2022-09-16 01:09:03 +0300

    base: WeatherTile: remove till it's fixed
    frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.
    java:56: error: symbol not found com.android.systemui.plugins.qs.DetailAdapter
    
    import com.android.systemui.plugins.qs.DetailAdapter;
    
    DetailAdapter.java was removed here: https://github.com/AICP/frameworks_base/commit/c292eff31f6b99a7d48caa1efb49800080d641d3
    
    Change-Id: Ie78d7ed5d6eff9797bafb00a9817bdf67a6ccecb

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -59,7 +59,6 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
-import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
@@ -109,7 +108,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<CPUInfoTile> mCPUInfoTileProvider;
     private final Provider<CompassTile> mCompassTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
-    private final Provider<WeatherTile> mWeatherTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -152,8 +150,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SmartPixelsTile> smartPixelsTileProvider,
             Provider<CPUInfoTile> cpuInfoTileProvider,
             Provider<CompassTile> compassTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider,
-            Provider<WeatherTile> weatherTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -192,7 +189,6 @@ public class QSFactoryImpl implements QSFactory {
         mCPUInfoTileProvider = cpuInfoTileProvider;
         mCompassTileProvider = compassTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
-        mWeatherTileProvider = weatherTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -278,8 +274,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mCompassTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
-            case "weather":
-                return mWeatherTileProvider.get();
         }
 
         // Custom tiles
