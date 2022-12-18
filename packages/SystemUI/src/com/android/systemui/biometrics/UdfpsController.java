6dcb378bd3a0e459a8fbd73b853f35ed5a4ca48e
ShevT <evgeny.shishkov@gmail.com>
2022-12-18 14:00:02 +0300

    fwb: Smart Pixels on UDFPS - drop disable_smart_pixels_on_udfps flag
     We don't need this flag.
    
    Change-Id: I60e2e47c0d05a154b153173dafd1adf9145943a5

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -202,7 +202,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private final SecureSettings mSecureSettings;
     private boolean mScreenOffUdfpsEnabled;
 
-    private boolean mDisableSmartPixels;
     private boolean mLowPowerMode = false;
     private boolean mSmartPixelsFlag = false;
     private int mSmartPixelsEnabled = 0;
@@ -228,9 +227,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private final ScreenLifecycle.Observer mScreenObserver = new ScreenLifecycle.Observer() {
         @Override
         public void onScreenTurnedOn() {
-            if (mDisableSmartPixels) {
-                isSmartPixelsEnabled();
-            }
+            isSmartPixelsEnabled();
             mScreenOn = true;
             if (mAodInterruptRunnable != null) {
                 mAodInterruptRunnable.run();
@@ -842,7 +839,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
         mUdfpsVendorCode = context.getResources().getInteger(R.integer.config_udfpsVendorCode);
-        mDisableSmartPixels = mContext.getResources().getBoolean(com.android.internal.R.bool.disable_smart_pixels_on_udfps);
         mAmbientDisplayConfiguration = new AmbientDisplayConfiguration(mContext);
         mSecureSettings = secureSettings;
         updateScreenOffUdfpsState();
@@ -1137,7 +1133,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mExecution.assertIsMainThread();
 
         if (!mSmartPixelsFlag) {
-            if (mDisableSmartPixels && ((mSmartPixelsEnabled == 1) || ((mSmartPixelsOnPowerSave == 1) && mLowPowerMode))) {
+            if ((mSmartPixelsEnabled == 1) || ((mSmartPixelsOnPowerSave == 1) && mLowPowerMode)) {
                 disableSmartPixels();
             }
             mSmartPixelsFlag = true;
@@ -1237,7 +1233,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mActivePointerId = -1;
         mAcquiredReceived = false;
 
-        if (mDisableSmartPixels && ((mSmartPixelsEnabled == 1) || ((mSmartPixelsOnPowerSave == 1) && mLowPowerMode))) {
+        if ((mSmartPixelsEnabled == 1) || ((mSmartPixelsOnPowerSave == 1) && mLowPowerMode)) {
             enableSmartPixels();
         }
         mSmartPixelsFlag = false;
