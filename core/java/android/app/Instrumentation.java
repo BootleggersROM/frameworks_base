b2d6f01966ab15806e8e8ff9be7954e1b8d27a38
El Dainosor <eldainosor@gmail.com>
2022-12-20 17:27:40 -0300

    core: Revert current PixelPropUtils imp
    This revert the following changes:
    b1c3de7f2c38d661f90d0f08e73fa61d56ec8e66
    20007e8f05ecc54838de1257c7df2830d755488a
    e2618d88d97ee111e456f46cdf645da51e594945

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,7 +56,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.PixelPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1243,7 +1242,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1261,7 +1259,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 
