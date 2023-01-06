41fc1313e7209b9536f2564fd22f96a3b0073334
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-06 22:13:07 +0200

    base: Allow setting wake for notification separately from doze [1/2]
    The strings were misleading. Disabling wake for notifications actually entirely disables doze.
    Add a new setting to configure wake for notification separately and amend doze switch strings accordingly

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11825,6 +11825,12 @@ public final class Settings {
         public static final String DOZE_ON_CHARGE = "doze_on_charge";
 
         /**
+         * Whether to trigger doze for new notifications
+         * @hide
+         */
+        public static final String DOZE_FOR_NOTIFICATIONS = "doze_for_notifications";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
