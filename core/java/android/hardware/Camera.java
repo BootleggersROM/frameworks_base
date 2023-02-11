0ada69bbb2b68283e45fc11bd37e6e7a0e63531f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-02-11 04:15:16 +0000

    CameraManager: Handle NPE with ActivityThread.currentOpPackageName()
    Arguably we should get the package name from context instead of
    ActivityThread, but passing context to these static functions
    is a pain in the ass.
    
    11-18 22:51:36.468 W/Binder  (10072): Caught a RuntimeException from the binder stub implementation.
    11-18 22:51:36.468 W/Binder  (10072): java.lang.NullPointerException: Attempt to invoke virtual method 'boolean java.lang.String.equals(java.lang.Object)' on a null object reference
    11-18 22:51:36.468 W/Binder  (10072): 	at android.hardware.camera2.CameraManager$CameraManagerGlobal.onTorchStatusChangedLocked(CameraManager.java:2401)
    11-18 22:51:36.468 W/Binder  (10072): 	at android.hardware.camera2.CameraManager$CameraManagerGlobal.onTorchStatusChanged(CameraManager.java:2542)
    11-18 22:51:36.468 W/Binder  (10072): 	at android.hardware.ICameraServiceListener$Stub.onTransact(ICameraServiceListener.java:167)
    11-18 22:51:36.468 W/Binder  (10072): 	at android.os.Binder.execTransactInternal(Binder.java:1285)
    11-18 22:51:36.468 W/Binder  (10072): 	at android.os.Binder.execTransact(Binder.java:1244)
    
    Change-Id: I8716888df114585b99d8d1c3070dbc1c86ccaf05

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -292,13 +292,13 @@ public class Camera {
          * if the package name does not falls in this bucket
          */
         String packageName = ActivityThread.currentOpPackageName();
-    	if (packageName == null)
+    	if (packageName == null) {
     	    return true;
+        }
         List<String> packageList = Arrays.asList(
                 SystemProperties.get("vendor.camera.aux.packagelist", packageName).split(","));
         List<String> packageExcludelist = Arrays.asList(
                 SystemProperties.get("vendor.camera.aux.packageexcludelist", "").split(","));
-
         return packageList.contains(packageName) && !packageExcludelist.contains(packageName);
     }
 
